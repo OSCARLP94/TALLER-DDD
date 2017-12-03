15f48c17be59391fc4ecb259b699939d80dab636
OSCARLP94 <33301459+OSCARLP94@users.noreply.github.com>
2017-12-03 13:50:34 -0500

    andale
    andale

--- Application.Test/SesionCatadoServiceTest.cs
+++ Application.Test/SesionCatadoServiceTest.cs
@@ -24,8 +24,8 @@ namespace Application.Test
             _mockUnitWork = new Mock<IUnitOfWork>();
             _service = new SesionCatadoService(_mockUnitWork.Object, _mockRepository.Object);
             listSesionCatado = new List<SesionCatado>() {
-               new SesionCatado() { Id = "1", FechaInicio = new DateTime(2008, 6, 1, 7, 47, 0), Descripcion="", IdCiudad="311", Identificador="Digitos", Protocolo="Arabica", Muestras= 1 , IdPerson="201", Estado="Incompleto"},
-               new SesionCatado() { Id = "2", FechaInicio = new DateTime(2008, 6, 1, 7, 47, 0), Descripcion="", IdCiudad="313", Identificador="Letras", Protocolo="Arabica", Muestras= 1, IdPerson="201", Estado="Incompleto" }
+               new SesionCatado() { Id = "1", FechaInicio = new DateTime(2008, 6, 1, 7, 47, 0), Descripcion="", IdCiudad="311", Identificador="Digitos", Protocolo="Arabica", CantidadMuestras= 1 , IdPerson="201", Estado="Incompleto"},
+               new SesionCatado() { Id = "2", FechaInicio = new DateTime(2008, 6, 1, 7, 47, 0), Descripcion="", IdCiudad="313", Identificador="Letras", Protocolo="Arabica", CantidadMuestras= 1, IdPerson="201", Estado="Incompleto" }
             };
         }
 
@@ -48,7 +48,7 @@ namespace Application.Test
         {
             //Arrange
             string Id = "3";
-            SesionCatado emp = new SesionCatado() { FechaInicio = new DateTime(2008, 6, 1, 7, 47, 0), IdCiudad = "311", Identificador = "Digitos", Protocolo = "Arabica", Muestras = 1, IdPerson = "201", Estado = "Incompleto" };
+            SesionCatado emp = new SesionCatado() { FechaInicio = new DateTime(2008, 6, 1, 7, 47, 0), IdCiudad = "311", Identificador = "Digitos", Protocolo = "Arabica", CantidadMuestras = 1, IdPerson = "201", Estado = "Incompleto" };
             _mockRepository.Setup(m => m.Add(emp)).Returns((SesionCatado e) =>
             {
                 e.Id = Id;
